5d6840e7d9fe76b4ea017a660d0fc83ae2462571
thatseant <46109197+thatseant@users.noreply.github.com>
2020-08-25 13:01:46 +0800

    Stores and displays user inputs when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,17 +7,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        //Welcome Message
+        //Prints welcome message on start
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        //Echoes commands
+        //Adds user inputs to storedUserInputs Array
         Scanner in = new Scanner(System.in);
+        String[] storedUserInputs = new String[100];
+        int listLength = 0;
         String userInput = in.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(userInput);
-            userInput = in.nextLine();
+            if (userInput.equals("list")) { //Displays previous user inputs
+                for (int i = 0; i< listLength; i++) {
+                    System.out.format("%d: %s%n", i, storedUserInputs[i]);
+                }
+            } else {
+                System.out.format("added: %s%n", userInput);
+                storedUserInputs[listLength] = userInput;
+                listLength++;
+            }
+                userInput = in.nextLine();
         }
 
         //Exits message when user says bye
