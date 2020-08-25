7b913edf31884a22fe1c92b67defa9eb0962c931
thatseant <46109197+thatseant@users.noreply.github.com>
2020-08-25 12:10:22 +0800

    Echoes commands entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,9 +6,21 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        //Welcome Message
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
+        //Echoes commands
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = in.nextLine();
+        }
+
+        //Exits message when user says bye
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
